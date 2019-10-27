b899c987933dcade35bb9c5b8c2a99d7ba5cc496
Nikola Tasic <nikola.tasic.3698@metropolitan.ac.rs>
2019-10-28 00:04:17 +0100

    header scrollto

--- src/components/app/header/headerLink/HeaderLink.tsx
+++ src/components/app/header/headerLink/HeaderLink.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import "./HeaderLink.css"
+import "./HeaderLink.css";
 
 type HeaderLinkProps = {
 	href: string;
 	name: string;
 	icon: string;
-	target ?: string;
+	target?: string;
 }
 type HeaderLinkState = {
 	href: string;
 	name: string;
 	icon: string;
-	target ?: string;
+	target?: string;
 }
 
 export class HeaderLink extends React.Component {
@@ -24,9 +24,31 @@ export class HeaderLink extends React.Component {
 		this.state = {href: props.href, name: props.name, icon: props.icon, target: props.target};
 	}
 
+	handleClick(ev: React.MouseEvent) {
+		let target = (ev.target as HTMLAnchorElement).nodeName === "I" ? (ev.target as HTMLElement).parentElement as HTMLAnchorElement : ev.target as HTMLAnchorElement;
+		if (!target.attributes.getNamedItem("data-scrollto")!.value.startsWith("#"))
+			return true;
+		ev.preventDefault();
+		let element = document.querySelector(target.attributes.getNamedItem("data-scrollto")!.value) as HTMLDivElement;
+		if (element == null) {
+			window.scroll({
+				top: 0,
+				left: 0,
+				behavior: "smooth",
+			});
+		} else {
+			window.scroll({
+				top: element.offsetTop - 150,
+				left: 0,
+				behavior: "smooth",
+			});
+		}
+	}
+
 	render() {
 		return (
-			<a href={this.state.href} target={this.state.target} data-scrollto={this.state.href.startsWith("#") ? this.state.href : ""}
+			<a onClick={this.handleClick} href={this.state.href} target={this.state.target}
+			   data-scrollto={this.state.href.startsWith("#") ? this.state.href : ""}
 			   className="skill animated slideInUp faster white-text font-weight-bold">
 				<i className="material-icons orange-text">{this.state.icon}</i><br/>{this.state.name}
 			</a>
