13f58d15e563f18bf54585d52534abe9306c777e
Nikola Tasic <ntasic7@gmail.com>
2020-05-24 05:00:11 +0200

    fixed index routing

--- server-src/server.ts
+++ server-src/server.ts
@@ -15,9 +15,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use("/200", (req, res) => res.sendFile("index.html", {root: "public"}));
-app.use("/429", (req, res) => res.sendFile("index.html", {root: "public"}));
-app.use("/400", (req, res) => res.sendFile("index.html", {root: "public"}));
+app.use("/*", (req, res) => res.sendFile("index.html", {root: "public"}));
 app.use("/mail", mailController);
 
 app.listen(SERVER_PORT, () => console.log("Server running on port " + SERVER_PORT));
